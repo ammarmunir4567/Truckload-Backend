21d7a25516eb4a1378d5396cd99ed29eeec63333
okusjid <l200927@lhr.nu.edu.pk>
2024-07-31 12:12:37 +0500

    Updated the admin panel to display users

--- projectApp/admin.py
+++ projectApp/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ('username', 'email', 'role', 'is_staff', 'is_active', 'is_superuser')  # Add 'role' and any other fields you want to display
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('role',)}),  # Add custom fields in the admin detail view
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('role',)}),  # Add custom fields in the admin create user form
+    )
+
+# Register the admin class with the associated model
+admin.site.register(CustomUser, CustomUserAdmin)
