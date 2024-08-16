0283870be433291f1809fcab87dcc186a2642917
Ammar Munir <ammarmunir4567@gmail.com>
2024-08-16 23:11:16 +0500

    ready for vercel

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,11 @@
  echo "BUILD START"
  python3.12 -m pip install -r requirements.txt
  python3.12 manage.py collectstatic --noinput --clear
- echo "BUILD END"
+
+echo "Make Migration..."
+python3.12 manage.py makemigrations --noinput
+ python3.12 manage.py migrate --noinput
+
+echo "Collect Static..."
+python3.12 manage.py collectstatic --noinput --clear
+echo "BUILD END"
